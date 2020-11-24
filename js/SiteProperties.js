38ad428a96cb727a966447bff07fa44922679bff
keshavanandsingh <keshavanand22@gmail.com>
2020-11-25 01:49:31 +0530

    Added: Ability to retrive data from json server making GET call

--- js/SiteProperties.js
+++ js/SiteProperties.js
@@ -1,6 +1,6 @@
 let site_properties = {
-    use_local_storage:"true",
+    use_local_storage: "false",
     home_page: "../pages/homePage.html",
     add_emp_payroll_page: "../pages/Employee_payroll_form.html",
-    server_url:"http://localhost:3000/EmployeePayrollDB/"
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
 };
